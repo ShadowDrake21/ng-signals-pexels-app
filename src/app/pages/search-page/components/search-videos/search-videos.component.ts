67f0352e5199e341daf935a4c551e23c7b0afd70
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-06-21 10:44:34 +0300

    photos search

--- src/app/pages/search-page/components/search-videos/search-videos.component.ts
+++ src/app/pages/search-page/components/search-videos/search-videos.component.ts
@@ -2,12 +2,22 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-search-videos',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './search-videos.component.html',
   styleUrl: './search-videos.component.scss',
 })
-export class SearchVideosComponent {}
+export class SearchVideosComponent {
+  value = '';
+}
